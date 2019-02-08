f8e1b42103662a1bddf859e0f3be1759bc733f2a
StatsenkoAnna <39280473+StatsenkoAnna@users.noreply.github.com>
2019-02-08 09:44:19 +0300

    Update datetime.py

--- datetime.py
+++ datetime.py
@@ -1,13 +1,8 @@
-# Дата и время в python
+# Дата и время в python. Сколько мне дней сейчас. Когда мне исполнится 15000 дней
 
-from datetime import datetime  # сократит путь к классу
+from datetime import datetime  
 from datetime import timedelta
 
-# РАЗНИЦА в датах измеряется в ДНЯХ и СЕКУНДАХ.
-# нет понятия "МЕСЯЦ" или "ГОД", потому что их длина варьируется
-
-# 1970-01-01 - начало UNIX-эпохи
-                    #  Y  M  D  [h [m [s]]]
 birthday = datetime(1992, 11, 7)
 td = datetime.now() - birthday
 print("Мне " + str(td.days) + " дней")
@@ -16,7 +11,5 @@ othertd = timedelta(15000)  # создан интервал 15000 дней
 newdate = birthday + othertd
 print('Мне исполнится 15000 дней ' + str(newdate))
 
-# Посчитать, когда вам исполнится 15000 дней
-# узнать, сколько это будет полных лет
 
 
